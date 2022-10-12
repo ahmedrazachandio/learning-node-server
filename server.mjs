03a2aaf6661eed911a100e5bc34df872c1046a33
AhmedRaza <86877851+ahmedrazachandio@users.noreply.github.com>
2022-10-12 19:09:49 +0500

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,11 +1,46 @@
-import express from 'express'
-const app = express()
-const port = 3000
+// import express from 'express'
+// const app = express()
+// const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('This is Ahmed Raza Hello World!')
+// app.get('/', (req, res) => {
+//   res.send('This is Ahmed Raza Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+let todos = [];
+
+app.use(express.json());
+app.use(cors())
+
+app.post('/todo', (req, res) => {
+    
+    todos.push(req.body.text);
+
+    res.send({
+        message: "your todo is saved",
+        data: todos
+    })
+})
+app.get('/todos', (req, res) => {
+    
+    res.send({
+        message: "here is you todo list",
+        data: todos
+    })
 })
 
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
